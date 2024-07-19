d254c3e6e9673f1719f381e29c54c094e85e98d1
lzy <lha602@163.com>
2024-07-20 00:52:16 +0800

    RPC核心实现

---
+++ rpc-core/src/main/java/com/lzy/rpc/consumer/proxy/ServiceProxyFactory.java
@@ -0,0 +1,23 @@
+package com.lzy.rpc.consumer.proxy;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * 服务代理工厂（用于创建代理对象）
+ */
+public class ServiceProxyFactory {
+
+    /**
+     * 根据服务类获取代理对象
+     *
+     * @param serviceClass
+     * @param <T>
+     * @return
+     */
+    public static <T> T getProxy(Class<T> serviceClass) {
+        return (T) Proxy.newProxyInstance(
+                serviceClass.getClassLoader(),
+                new Class[]{serviceClass},
+                new ServiceProxy());
+    }
+}
