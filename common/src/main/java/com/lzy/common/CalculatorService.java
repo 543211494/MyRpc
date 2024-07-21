0e3e8688121550acbbacd39a34efa4e566e10ad7
lzy <lha602@163.com>
2024-07-22 01:38:46 +0800

    公共接口

--- common/src/main/java/com/lzy/common/CalculatorService.java
+++ common/src/main/java/com/lzy/common/CalculatorService.java
@@ -2,10 +2,6 @@ package com.lzy.common;
 
 /**
  * 用户服务
- *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @learn <a href="https://codefather.cn">编程宝典</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 public interface CalculatorService {
 
