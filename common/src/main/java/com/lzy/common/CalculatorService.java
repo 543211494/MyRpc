f938d8bde78027440e4f7844dc8a079a2750d014
lzy <lha602@163.com>
2024-07-23 11:11:05 +0800

    公共接口

--- common/src/main/java/com/lzy/common/CalculatorService.java
+++ common/src/main/java/com/lzy/common/CalculatorService.java
@@ -9,11 +9,4 @@ public interface CalculatorService {
      * 获取用户
      */
     public int add(int a,int b);
-
-    /**
-     * 用于测试 mock 接口返回值
-     */
-    default short getNumber() {
-        return 1;
-    }
 }
